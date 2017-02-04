cf82b16a3aa89d3d05651c1d3f8e2ab575a63cd8
Jonathan Basden <Jonathan Basden>
2017-02-03 22:46:28 -0600

    Adding final touches to game

--- src/js/components/View.js
+++ src/js/components/View.js
@@ -5,13 +5,15 @@ export default class View extends React.Component {
 		var boxes = [];
 		for(var i = 1; i <= 9; i++) {
 			var className = "box";
+			var text = "";
 			if(this.props.clickedBoxes.indexOf(i) >= 0) {
 				className += this.props.grid[i] === 0? " clicked-x" : " clicked-o";
+				text = this.props.grid[i] === 0? "X" : "O";
 			}
 			else {
 				className += this.props.player === 0? " x-hover" : " o-hover";
 			}
-			boxes.push(<div key={i} value={i} className={className} onClick={this.props.handleClick.bind(this, i)} ></div>);
+			boxes.push(<div key={i} value={i} className={className} onClick={this.props.handleClick.bind(this, i)}>{text}</div>);
 		}
 
 		var className = "btn btn-primary",
@@ -19,15 +21,15 @@ export default class View extends React.Component {
 
 		if(this.props.xWon) {
 			className += " x-won";
-			text = "X Won!";
+			text = "X Won! New Game?";
 		}
 		else if(this.props.oWon) {
 			className += " o-won";
-			text = "O Won!";
+			text = "O Won! New Game?";
 		}
 		else if(this.props.draw) {
 			className += " draw";
-			text = "Draw!";
+			text = "Draw! New Game?";
 		}
 		else {
 			className += " hidden";
@@ -40,6 +42,7 @@ export default class View extends React.Component {
 
 		return (
 			<div>
+				<div className="jumbotron" id="header"><strong>Tic Tac Toe</strong></div>
 				<div id="grid" className={gridClass} >{boxes}</div>
 				<button className={className} onClick={this.props.resetGame}>{text}</button>
 			</div> 
